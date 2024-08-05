69134ee06c614532159d79953dbf4fd134a86300
Anthony <anthony.onyango@student.moringaschool.com>
2024-08-06 00:29:16 +0300

    added the state variables

---
+++ src/Components/Transactions.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+//function will be called when form is submitted
+const Transactions= ({onSubmit})=> {
+
+    //initialized the state variables
+    const [category, setCategory] = useState('');
+    const [description, setDescription] = useState('');
+    const [amount, setAmount] = useState('');
+
+
+    
+}
