bacfc4e7f8fb10de1c4ad0128433c5774fc190d2
Anthony <anthony.onyango@student.moringaschool.com>
2024-08-06 00:57:43 +0300

    implemented form adding the transactions with date, category, description and the amount

--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -10,15 +10,50 @@ const Transactions= ({onSubmit})=> {
 
    
     
-//     //function to handle form submit
-//     const handleSubmit = (e) => {
-//     e.preventDefault();//prevents default form behavior
+    //function to handle form submit
+    const handleSubmit = (e) => {
+    e.preventDefault();//prevents default form behavior
 
-//     //calls onSubmit prop with the current state values
-//     onSubmit({category, description, amount});
-//     setCategory('');
-//     setDescription('');
-//     setAmount('');
-// }
-    
+    //calls onSubmit prop with the current state values
+    onSubmit({category, description, amount});
+    setCategory('');
+    setDescription('');
+    setAmount('');
 }
+
+// //return form element with onSubmit event handler
+// return (
+//     <form onSubmit={handleSubmit}>
+        
+//         <input type="text"
+//         placeholder="Date"
+//         value={date}
+//         onChange={(e) => setDate(e.target.value)}
+//         />
+
+//         <input type="text"
+//         placeholder="Input Category"
+//         value={category}
+//         onChange={(e) => setCategory(e.target.value)}
+//         />
+
+//         <input type="text"
+//         placeholder="Description"
+//         value={description}
+//         onChange={(e) => setDescription(e.target.value)}
+//         />
+
+//         <input type="text"
+//         placeholder="Amount"
+//         value={amount}
+//         onChange={(e) => setAmount(e.target.value)}
+//         />
+
+//         <button type="submit">Add Transaction</button>
+
+//     </form>
+// );
+    
+// };
+
+export default Transactions;
