c3faa8b61dac90c4e4a32b0b66ca309c0d001fec
Anthony <anthony.onyango@student.moringaschool.com>
2024-08-06 08:39:20 +0300

    implemented main application layout with transaction, filter and table components

--- src/App.js
+++ src/App.js
@@ -15,4 +15,19 @@ const handleFilterTransactions = (term) => {
   setFilterTerm(term);
 };
 
-}
+const filteredTransactions = transactions.filter((transaction) => transaction.description.toLowerCase())
+};
+
+return (
+  <div>
+    <h1>Flatiron Bank</h1>
+    {/*renders the transaction component and passes the handleAddTransaction function as a prop*/}
+    <Transaction onSubmit={handleAddTransaction} />
+        
+        {/*renders the filter component and passes the handleFilterTransactions function as a prop*/}
+        <Filter onFilter={handleFilterTransactions} />
+
+            {/*renders the table component and passes the filteredTransactions array as a prop*/}
+            <Table transactions={filteredTransactions} />
+  </div>
+)
